266be3864df2a13046907e356e651351fc1d3e41
Travis Miller <puckbag@gmail.com>
2012-04-03 17:11:31 -0500

    Pull that file data from git rather than hack-y bash output.

--- kohana-setup.sh
+++ kohana-setup.sh
@@ -2,63 +2,8 @@
 
 # http://kohanaframework.org/3.2/guide/kohana/tutorials/git
 
-# http://stackoverflow.com/a/8088167
-define(){ IFS='\n' read -r -d '' ${1}; }
-
-
-##########################################################################################
-# File: example.htaccess
-#----------------------------------------------------------------------------------------#
-define HTACCESS <<'EOF'
-# Production or Development environment
-SetEnv KOHANA_ENV DEVELOPMENT
-#SetEnv KOHANA_ENV PRODUCTION
-
-# Turn on URL rewriting
-RewriteEngine On
-
-# Installation directory
-RewriteBase /
-
-# Protect hidden files from being viewed
-<Files .*>
-        Order Deny,Allow
-        Deny From All
-</Files>
-
-# Protect application and system files from being viewed
-RewriteRule ^(?:application|modules|system)\b.* index.php/$0 [L]
-
-# Allow any files or directories that exist to be displayed directly
-RewriteCond %{REQUEST_FILENAME} !-f
-RewriteCond %{REQUEST_FILENAME} !-d
-
-# Rewrite all other URLs to index.php/URL
-RewriteRule .* index.php/$0 [PT]
-EOF
-#----------------------------------------------------------------------------------------#
-
-
-##########################################################################################
-# File: .gitignore
-#----------------------------------------------------------------------------------------#
-define GITIGNORE <<'EOF'
-.htaccess
-EOF
-#----------------------------------------------------------------------------------------#
-
-
-##########################################################################################
-# File: application/config/.gitignore
-#----------------------------------------------------------------------------------------#
-define APPLICATION_CONFIG_GITIGNORE <<'EOF'
-*.php
-EOF
-#----------------------------------------------------------------------------------------#
-
-
 PROJECT="$1"
-CWD=`cwd`
+CWD=`pwd`
 
 mkdir "$PROJECT"
 cd "$PROJECT"
@@ -82,12 +27,11 @@ mkdir -m 0777 -p application/{cache,logs}
 echo '[^.]*' > application/logs/.gitignore
 echo '[^.]*' > application/cache/.gitignore
 
+wget https://github.com/puckbag/kohana-kohana/raw/3.2/puckbag/.gitignore --no-check-certificate
+wget https://github.com/puckbag/kohana-kohana/raw/3.2/puckbag/example.htaccess --no-check-certificate
 wget https://github.com/puckbag/kohana-kohana/raw/3.2/puckbag/index.php --no-check-certificate
 wget https://github.com/puckbag/kohana-kohana/raw/3.2/puckbag/application/bootstrap.php --no-check-certificate -O application/bootstrap.php
-
-echo "$HTACCESS" > example.htaccess
-echo "$GITIGNORE" > .gitignore
-echo "$APPLICATION_CONFIG_GITIGNORE" > application/config/.gitignore
+wget https://github.com/puckbag/kohana-kohana/raw/3.2/puckbag/application/config/.gitignore --no-check-certificate -O application/config/.gitignore
 
 git add .
 git commit -m 'Added initial directory structure'
